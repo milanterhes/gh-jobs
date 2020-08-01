8943e4902b84821be6d278553ca4743e7ed1aa0b
Milán Terhes <terhes.milan@gmail.com>
2020-08-01 13:55:42 +0200

    some filters

--- src/components/FilterCollection.tsx
+++ src/components/FilterCollection.tsx
@@ -4,10 +4,42 @@ import styled from 'styled-components';
 const Collection = styled.div`
     width: 45%;
     height: 100px;
+    background: white;
+    margin-top: 16px;
 `
 
+type FilterCheckboxProps = {
+    label: string;
+}
+
+const FilterCheckbox = ({ label }: FilterCheckboxProps) => {
+    const Label = styled.label`
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 21px;
+        color: #334680;
+        margin-left: 12px;
+    `
+    return (
+        <React.Fragment>
+            <input type="checkbox" name={`FilterCheckbox-${label}`} />
+            <Label htmlFor={`FilterCheckbox-${label}`}>{label}</Label>
+        </React.Fragment>
+    )
+}
+
 const FilterCollection = () => (
-    <Collection></Collection>
+    <Collection>
+        <div id="type">
+            <FilterCheckbox label={"Full time"} />
+        </div>
+        <div id="location">
+            <div id="location-search"></div>
+            <div id="locations"></div>
+        </div>
+    </Collection>
 )
 
 export default FilterCollection;
